792d24cd28fdd2b679598f7baeb4883987b24b75
Toby Sims <tsims@mailjet.com>
2015-02-09 19:01:06 +0000

    git more things

--- web.js
+++ web.js
@@ -7,3 +7,14 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+// var express = require('express');
+// var port = process.env.PORT || 3000;
+// var app = express.createServer();
+ 
+// app.get('/', function(request, response) {
+//     response.sendfile(__dirname + '/index.html');
+// }).configure(function() {
+//     app.use('/images', express.static(__dirname + '/images'));
+// }).listen(port);
