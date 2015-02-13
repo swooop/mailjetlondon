361d2f13b7c014dd80d20d2e5a3c7f75ff2a953b
Toby Sims <tsims@mailjet.com>
2015-02-13 11:36:00 +0000

    changed web.js

--- web.js
+++ web.js
@@ -1,20 +1,20 @@
-// var express = require("express");
-// var app = express();
-// app.use(express.logger());
-// app.use("/", express.static(__dirname));
+var express = require("express");
+var app = express();
+app.use(express.logger());
+app.use("/", express.static(__dirname));
 
-// var port = process.env.PORT || 5000;
-// app.listen(port, function() {
-//   console.log("Listening on " + port);
-// });
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 
 
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express.createServer();
+// var express = require('express');
+// var port = process.env.PORT || 3000;
+// var app = express.createServer();
  
-app.get('/', function(request, response) {
-    response.sendfile(__dirname + '/index.html');
-}).configure(function() {
-    app.use('/images', express.static(__dirname + '/images'));
-}).listen(port);
+// app.get('/', function(request, response) {
+//     response.sendfile(__dirname + '/index.html');
+// }).configure(function() {
+//     app.use('/images', express.static(__dirname + '/images'));
+// }).listen(port);
