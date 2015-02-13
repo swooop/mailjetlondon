cd11d5406145585dd8cb26e8f9bce8f3468a622e
Toby Sims <tsims@mailjet.com>
2015-02-13 12:05:31 +0000

    updated package.json

--- web.js
+++ web.js
@@ -1,7 +1,28 @@
-var express = require("express");
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var path = require('path');
+
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var errorHandler = require('errorhandler');
+var serveStatic = require('serve-static')
+
+
+
+
+
+
+
+
+
 var app = express();
 app.use(express.logger());
-app.use("/", express.static(__dirname));
+app.use("/", serveStatic(__dirname));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
